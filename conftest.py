0e32da1394a0d0234f22415ce2d9eb122eb23379
Heorhii Lazarevich <webremake@gmail.com>
2023-05-28 21:35:10 +0300

    implement auto generation of allure feature title based on test files names

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import os
+
+import allure
+import pytest
+
+
+@pytest.hookimpl(trylast=True, hookwrapper=True)
+def pytest_runtest_call(item):
+    """Allure dynamic title"""
+    yield
+    allure.dynamic.title(" ".join(item.name.split("_")[1:]).capitalize())
+
+    filename_with_extension = os.path.basename(item.location[0])
+    filename_without_extension = os.path.splitext(filename_with_extension)[0]
+    title = " ".join(filename_without_extension.split("_")[1:]).capitalize()
+    allure.dynamic.feature(title)
+
